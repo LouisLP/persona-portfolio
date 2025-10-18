3ccf1828e7d6b1a83d60215c30ee88c76f1ac19f
LouisLP <louis.lascelles.palys@gmail.com>
2025-10-18 19:03:59 +0200

    Polaroid containers for photos

--- src/components/Designer/DesignerContent.vue
+++ src/components/Designer/DesignerContent.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PolaroidPhoto from '@/components/Designer/PolaroidPhoto.vue'
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <div class="container w-full space-y-20 py-16">
+      <PolaroidPhoto />
+    </div>
+  </div>
 </template>
---
+++ src/components/Designer/PolaroidPhoto.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- Polaroid photo container -->
+  <figure
+    class="flex h-80 w-70 -rotate-2 items-center justify-center bg-gray-100 pb-10 md:h-110 md:w-100"
+  >
+    <!-- Image container -->
+    <div class="size-64 bg-gray-800 md:size-90"></div>
+  </figure>
+</template>
