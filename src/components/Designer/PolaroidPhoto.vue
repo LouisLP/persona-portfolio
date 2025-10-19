4009886170c92b3084917bd36c7a74a068da841e
LouisLP <louis.lascelles.palys@gmail.com>
2025-10-19 11:40:11 +0200

    Accepting props for all polaroids

--- src/components/Designer/PolaroidPhoto.vue
+++ src/components/Designer/PolaroidPhoto.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  imagePath: string
+  rotation?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  rotation: 0,
+})
+</script>
 
 <template>
   <!-- Polaroid photo container -->
   <figure
-    class="flex h-80 w-70 -rotate-2 items-center justify-center bg-gray-100 pb-10 md:h-110 md:w-100"
+    class="flex h-80 w-70 items-center justify-center bg-gray-100 pb-10 shadow-lg md:h-110 md:w-100"
+    :style="{ transform: `rotate(${props.rotation}deg)` }"
   >
     <!-- Image container -->
-    <div class="size-64 bg-gray-800 md:size-90"></div>
+    <div class="size-64 overflow-hidden bg-gray-800 md:size-90">
+      <img :src="props.imagePath" :alt="'Polaroid photo'" class="size-full object-cover" />
+    </div>
   </figure>
 </template>
